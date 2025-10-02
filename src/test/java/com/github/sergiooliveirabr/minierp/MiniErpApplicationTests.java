cc1f2138d6a48712033f71465c1a8c931be8623b
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-10-02 11:57:18 +0100

    project created

---
+++ src/test/java/com/github/sergiooliveirabr/minierp/MiniErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sergiooliveirabr.minierp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
