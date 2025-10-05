afa374161c83da3408579e576bbc9dae2413e037
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-10-05 17:10:11 +0100

    Update IndexController.java
    act:
    -> name refactored (pipeline test)

--- src/main/java/com/github/sergiooliveirabr/minierp/controller/IndexController.java
+++ src/main/java/com/github/sergiooliveirabr/minierp/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/index")
-public class Index {
+public class IndexController {
 
     @GetMapping()
     public String indexView() {
