57a54fdcb31c07d0b9fa252ba383bf48b15fdf69
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-10-07 09:07:45 +0100

    Create PipeplineTest.java
    act: ->
    this was created to test the pipeline test phase

---
+++ src/test/java/com/github/sergiooliveirabr/minierp/PipeplineTest.java
@@ -0,0 +1,14 @@
+package com.github.sergiooliveirabr.minierp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+public class PipeplineTest {
+
+    @Test
+    void TestingMyPipeline() {
+        fail("This is just a test");
+    }
+}
