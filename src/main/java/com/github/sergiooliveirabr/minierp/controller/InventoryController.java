befc932a6e4e5e27dbfb3737f4ebf90cb0282c21
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-10-08 21:38:11 +0100

    Update InventoryController.java
    act:
    -> method post to insert item created

--- src/main/java/com/github/sergiooliveirabr/minierp/controller/InventoryController.java
+++ src/main/java/com/github/sergiooliveirabr/minierp/controller/InventoryController.java
@@ -1,15 +1,34 @@
 package com.github.sergiooliveirabr.minierp.controller;
 
+import com.github.sergiooliveirabr.minierp.entity.Item;
+import com.github.sergiooliveirabr.minierp.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/inventory")
 public class InventoryController {
 
+    private final ItemService itemService;
+
+    @Autowired
+    public InventoryController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
     @GetMapping
-    public String inventoryView() {
+    public String inventoryView(Model model) {
+
+        // Adds the empty Item object to the CREATE form (th:object="${item}")
+        model.addAttribute("item", new Item());
         return "inventory";
+    }
+
+    @PostMapping("/insert")
+    public String insertItem(@ModelAttribute("item") Item item) {
+        itemService.save(item);
+        return "redirect:/inventory";
     }
 }
