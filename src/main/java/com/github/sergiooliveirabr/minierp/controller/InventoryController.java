9583da2b326a9025f64a0dbccea776dde592c52f
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-10-08 20:31:39 +0100

    Update InventoryController.java
    approach changed
    the project is too complex
    
    act:
    -> page refactored from product to inventory
    -> inventory will be the main feature

--- src/main/java/com/github/sergiooliveirabr/minierp/controller/InventoryController.java
+++ src/main/java/com/github/sergiooliveirabr/minierp/controller/InventoryController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/products")
-public class ProductController {
+@RequestMapping("/inventory")
+public class InventoryController {
 
     @GetMapping
-    public String productView() {
+    public String inventoryView() {
         return "inventory";
     }
 }
