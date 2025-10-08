6424e348a56cf1c3e37a7f1982feeb773995fe65
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-10-08 16:27:42 +0100

    Update MiniErpApplicationTests.java
    act:
    -> annotation added to invoke the profile created called test

--- src/test/java/com/github/sergiooliveirabr/minierp/MiniErpApplicationTests.java
+++ src/test/java/com/github/sergiooliveirabr/minierp/MiniErpApplicationTests.java
@@ -2,7 +2,9 @@ package com.github.sergiooliveirabr.minierp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class MiniErpApplicationTests {
 
