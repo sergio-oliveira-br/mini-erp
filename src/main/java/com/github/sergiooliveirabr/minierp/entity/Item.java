e6f361b6407cc6ff82343daa16f8fbcd7879b288
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-10-08 21:46:42 +0100

    Update Item.java
    cleanup
    
    -> lombok residue

--- src/main/java/com/github/sergiooliveirabr/minierp/entity/Item.java
+++ src/main/java/com/github/sergiooliveirabr/minierp/entity/Item.java
@@ -1,8 +1,6 @@
 package com.github.sergiooliveirabr.minierp.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
