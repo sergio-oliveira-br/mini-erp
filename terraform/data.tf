c8b74bac5639df05183791d1880ae825e555c0d0
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-10-14 08:53:38 +0100

    Update data.tf
    act:
    -> phase 3 created (deploy)

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,4 +1,4 @@
-# terraform/data.tf
+# # terraform/data.tf
 
 # Search the ECR Repository
 data "aws_ecr_repository" "app" {
@@ -8,4 +8,25 @@ data "aws_ecr_repository" "app" {
 # Search IAM Role for CodeBuild
 data "aws_iam_role" "codebuild_role" {
   name = "mini-erp-codebuild-role"
+}
+
+# # Search the default VPC (or your existing VPC)
+# data "aws_vpc" "default" {
+#   filter {
+#     name = "is-default"
+#     values = [true]
+#   }
+# }
+#
+# # Search the subnets (sub-networks) of your VPC (for Fargate)
+# data "aws_subnets" "default" {
+#   filter {
+#     name = "vpc-id"
+#     values = [data.aws_vpc.default.id]
+#   }
+# }
+
+# Search AZs
+data "aws_availability_zones" "available" {
+  state = "available"
 }
