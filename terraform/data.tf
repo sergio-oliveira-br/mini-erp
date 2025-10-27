618ab32b6ef41a432b22557f6155005b34f91a31
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-10-27 23:22:51 +0000

    Update data.tf
    act:
    -> cleanup

--- terraform/data.tf
+++ terraform/data.tf
@@ -10,22 +10,6 @@ data "aws_iam_role" "codebuild_role" {
   name = "mini-erp-codebuild-role"
 }
 
-# # Search the default VPC (or your existing VPC)
-# data "aws_vpc" "default" {
-#   filter {
-#     name = "is-default"
-#     values = [true]
-#   }
-# }
-#
-# # Search the subnets (sub-networks) of your VPC (for Fargate)
-# data "aws_subnets" "default" {
-#   filter {
-#     name = "vpc-id"
-#     values = [data.aws_vpc.default.id]
-#   }
-# }
-
 # Search AZs
 data "aws_availability_zones" "available" {
   state = "available"
