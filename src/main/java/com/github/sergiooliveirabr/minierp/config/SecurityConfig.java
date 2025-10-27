51ea9ba5fc7e18e4afd179836a136aab4a9eb77f
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-10-27 12:54:59 +0000

    Create SecurityConfig.java
    Act:
    -> security method created - login and restrictions access eliminated

---
+++ src/main/java/com/github/sergiooliveirabr/minierp/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.github.sergiooliveirabr.minierp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            );
+
+        return http.build();
+    }
+}
